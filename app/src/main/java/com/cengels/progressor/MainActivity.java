7c1c612a25f0c248acd7e76bc82ea1716a41544a
Christopher Engels <christopherengels.de@googlemail.com>
2019-07-20 19:34:55 +0200

    Set theme colors

--- app/src/main/java/com/cengels/progressor/MainActivity.java
+++ app/src/main/java/com/cengels/progressor/MainActivity.java
@@ -1,13 +1,12 @@
 package com.cengels.progressor;
 
 import android.os.Bundle;
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
